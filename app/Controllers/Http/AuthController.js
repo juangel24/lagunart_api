8eed1d26b54c4f45fb6af403fa09babb7ca14de5
Juan Angel <juangel2406@gmail.com>
2020-07-25 19:17:43 -0500

    logo and static path

---
+++ app/Controllers/Http/AuthController.js
@@ -0,0 +1,40 @@
+'use strict'
+
+class AuthController {
+  async login({ request, auth }) {
+    const { email, password} = request.all()
+
+    if (await auth.attempt(email, password)) {
+      let user = await User.findBy('email', email)
+      let token = await auth.generate(user)
+
+      return this._user(token, user)
+    }
+  }
+
+  async logout() { await auth.logout() }
+
+  async signup({ request, auth}) {
+    const data = request.only(['username', 'email', 'password'])
+
+    // looking for user in database
+    let user = await User.findBy('username', data.username)
+    if (user) return { msg: 'username taken' }
+
+    // looking for email in database
+    user = await User.findBy('email', data.email)
+    if (user) return { msg: 'email taken' }
+
+    user = await User.create(data)
+    let token = await auth.generate(user)
+
+    return this._user(token, user)
+  }
+
+  async _user(token, user) {
+    Object.assign(user, token)
+    return user
+  }
+}
+
+module.exports = AuthController
