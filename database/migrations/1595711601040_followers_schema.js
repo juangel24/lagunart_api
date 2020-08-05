92bad607f09f4316e2a8b656e4451b672c6f372d
Juan Angel <juangel2406@gmail.com>
2020-08-04 23:06:44 -0500

    followers ignore vscode user folders

--- database/migrations/1595711601040_followers_schema.js
+++ database/migrations/1595711601040_followers_schema.js
@@ -6,10 +6,9 @@ const Schema = use('Schema')
 class FollowersSchema extends Schema {
   up () {
     this.create('followers', (table) => {
-      table.increments()
     	table.integer('follower').unsigned().references('id').inTable('users').notNullable()
-    	table.integer('followed').unsigned().references('id').inTable('users').notNullable()
-    	table.boolean('is_followed').notNullable()
+    	table.integer('user_id').unsigned().references('id').inTable('users').notNullable()
+    	table.boolean('is_followed').defaultTo(1)
     })
   }
 
