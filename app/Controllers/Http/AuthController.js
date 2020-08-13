109ca591bc4e87bde94929f9f292f88d6461f354
Angel <juaangel.rey@gmail.com>
2020-08-13 15:42:31 -0500

    espanich

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -13,11 +13,11 @@ class AuthController {
 
     // if username is already taken
     let user = await User.findBy('username', data.username)
-    if (user) { return response.conflict('username') }
+    if (user) { return response.conflict('usuario') }
 
     // if email already exists
     user = await User.findBy('email', data.email)
-    if (user) { return response.conflict('email') }
+    if (user) { return response.conflict('correo') }
 
     user = await User.create(data)
     return this._userWithToken(auth, user)
